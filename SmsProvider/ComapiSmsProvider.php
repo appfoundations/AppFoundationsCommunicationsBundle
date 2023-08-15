0c0a65ac4e3cf3b2ea07b255ed999a34c96d6712
Alexandre Paz <erzapito@gmail.com>
2023-08-15 13:47:17 +0200

    Release V0.0.9

--- SmsProvider/ComapiSmsProvider.php
+++ SmsProvider/ComapiSmsProvider.php
@@ -22,9 +22,9 @@ class ComapiSmsProvider implements ISmsProvider
     private $apiSpace;
     private $token;
     private $senderId;
-
+    
     public function __construct( $config) {
-
+        
         $this->apiSpace = $config['comapi']['space'];
         $this->token = $config['comapi']['token'];
         $this->senderId = $config['comapi']['sender'];
@@ -32,51 +32,51 @@ class ComapiSmsProvider implements ISmsProvider
             $this->senderId = "Not set";
         }
     }
-
+    
     public function sendSms($dst, $content, \DateTime $schedule = NULL, $sendAs = NULL)
     {
         $fullRecipient = (strlen($dst) < 12) ? ("44".$dst) : $dst;
-
-
+        
+        
         $handler = new StreamHandler();
         $stack = HandlerStack::create($handler);
         $client = new Client(['handler' => $stack]);
-
-
+        $body = [
+            'body' => urldecode($content),
+            'to' => ['phoneNumber' => $fullRecipient],
+            'channelOptions' => [
+                'sms' => ['from' => (isset($sendAs) && !empty($sendAs)) ? $sendAs : $this->senderId]
+            ],
+            'rules' => ['sms']
+        ];
+        
         try {
             $response = $client->request('POST',"https://api.comapi.com/apispaces/{$this->apiSpace}/messages", [
                 'headers' => [
                     'cache-control' => 'no-cache',
                     'Accept'     => 'application/json',
                     'Authorization' => "Bearer {$this->token}"
-                ],
-                'json' => [
-                    'body' => urldecode($content),
-                    'to' => ['phoneNumber' => $fullRecipient],
-                    'channelOptions' => [
-                        'sms' => ['from' => (isset($sendAs) && !empty($sendAs)) ? $sendAs : $this->senderId]
                     ],
-                    'rules' => ['sms']
-                ]
-            ]);
-
+                    'json' => $body
+                    ]);
+            
             $code = $response->getStatusCode(); // 200
             $reason = $response->getReasonPhrase(); // OK
-
-
+            
+            
             return [
                 'status' => 'OK',
                 'details' => ['code' => $code,'reason' => $reason, 'dst' => $fullRecipient, 'content' => $content]
-                ];
+            ];
         } catch (RequestException $e) {
-            $requestStr = Psr7\str($e->getRequest());
+            $requestStr = var_export($e->getRequest(), true);
             $responseStr = null;
             if ($e->hasResponse()) {
-                $responseStr = Psr7\str($e->getResponse());
+                $responseStr = var_export($e->getResponse(), true);
             }
             return [
                 'status' => 'NOK',
-                'details' => ['request' => $requestStr, 'response' => $responseStr]
+                'details' => ['request' => $requestStr, 'response' => $responseStr, $body => $body]
             ];
         }
     }
